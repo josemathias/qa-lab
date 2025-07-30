9549ef2dedb4a25f0b7846e703cc6b32c7280f89
Jose Mathias <jose.mathiasg@gmail.com>
2025-07-30 18:21:57 -0300

    Suppressed the support file for cypress

--- L1-integration-tests/cypress.config.js
+++ L1-integration-tests/cypress.config.js
@@ -3,5 +3,9 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://google.com',
+    setupNodeEvents(on, config) {
+      // whatever setup you want
+    },
+    supportFile: false, // 🔥 disables the support file requirement
   },
 });
