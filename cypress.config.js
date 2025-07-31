1f0e725b20d10f9e3eb02e54ed536423900bfd02
Jose Mathias <jose.mathiasg@gmail.com>
2025-07-31 15:55:47 -0300

    Trigger cypress tests

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "72589d73-a9c5-4663-b55a-e89dde000fc1",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
